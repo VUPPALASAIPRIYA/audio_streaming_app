5a45b3a334e2c3a0b29bff40f280a2b21916821a
VUPPALASAIPRIYA <162854784+VUPPALASAIPRIYA@users.noreply.github.com>
2025-04-13 21:33:07 +0530

    Fixed deployment env

--- audio-streaming-app/audio-streaming-app/vite.config.js
+++ audio-streaming-app/audio-streaming-app/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:process.env.VITE_BASE_PATH || "/audio_streaming_app/tree/main/audio-streaming-app/audio-streaming-app"
 })
